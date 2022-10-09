04f0f7b82df7bd1832556ec34a223aff6c4d7669
Diego Hanssel Cabrales <diegohansselp24@gmail.com>
2022-10-08 20:37:47 -0600

    Commit inicial, creamos archivos js, html y css para darle forma al proyecto

---
+++ game.js
@@ -0,0 +1,13 @@
+const canvas = document.querySelector(`#game`);
+const game = canvas.getContext(`2d`);
+
+window.addEventListener(`load`, startGame);
+
+function startGame() {
+    window.innerHeight
+    window
+    // game.font = `25px Verdana`;
+    // game.fillStyle = `purple`;
+    // game.textAlign = `start`;
+    // game.fillText(`Puto`, 25, 25);
+}
